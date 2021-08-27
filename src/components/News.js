5c00dfe2260d1bb28252f1989ad9825ba678bd52
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2021-08-27 17:45:26 +0530

    Video 28 of course and 7 of project completed.

--- src/components/News.js
+++ src/components/News.js
@@ -7,18 +7,49 @@ export class News extends Component {
         super();
         this.state = {
             articles: [],
-            loading: false
+            loading: false,
+            page: 1
         };
     }
 
     async componentDidMount() {
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=537c688058604098a1781254864ef398";
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=537c688058604098a1781254864ef398&page=${this.state.page}&pageSize=20`;
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
         this.setState({
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults
+        });
+    }
+
+    handlePreviousClick = async () => {
+        console.log("Previous");
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=537c688058604098a1781254864ef398&page=${this.state.page + 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({
+            page: this.state.page - 1,
             articles: parsedData.articles
         });
+    }
+    
+    handleNextClick = async () => {
+        console.log("Next");
+        if(this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        }
+        else {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=537c688058604098a1781254864ef398&page=${this.state.page + 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({
+            page: this.state.page + 1,
+            articles: parsedData.articles
+        });
+    }
     }
     
     render() {
@@ -31,9 +62,11 @@ export class News extends Component {
                         <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url} />
                     </div>
                 })}
-                    
                 </div>
-
+            <div className="container d-flex justify-content-between">
+                <button type="button" disabled={this.state.page <= 1} className="btn btn-primary" onClick={this.handlePreviousClick}>&larr; Previous</button>
+                <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)} className="btn btn-primary" onClick={this.handleNextClick}>Next &rarr;</button>
+            </div>
             </div>
         )
     }
