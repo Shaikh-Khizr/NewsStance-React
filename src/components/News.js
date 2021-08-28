d09967e605d76dc67a2d52116e501260eed34318
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2021-08-28 22:21:04 +0530

    Video 33 of course and 12 of project completed.

--- src/components/News.js
+++ src/components/News.js
@@ -26,8 +26,8 @@ export class News extends Component {
         };
     }
 
-    async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=537c688058604098a1781254864ef398&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    async updateNews() {
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=537c688058604098a1781254864ef398&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({loading: true});
         let data = await fetch(url);
         let parsedData = await data.json();
@@ -37,35 +37,20 @@ export class News extends Component {
             loading: false
         });
     }
+    async componentDidMount() {
+        this.updateNews();
+    }
 
     handlePreviousClick = async () => {
-        console.log("Previous");
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=537c688058604098a1781254864ef398&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading: true});
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        this.setState({
-            page: this.state.page - 1,
-            articles: parsedData.articles,
-            loading: false
-        });
+        this.setState({page: this.state.page - 1});
+        this.updateNews();
     }
     
     handleNextClick = async () => {
-        console.log("Next");
-        if(!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=537c688058604098a1781254864ef398&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading: true});
-        let data = await fetch(url);
-        let parsedData = await data.json();
-        console.log(parsedData);
-        this.setState({
-            page: this.state.page + 1,
-            articles: parsedData.articles,
-            loading: false
-        });
+        this.setState({page: this.state.page + 1});
+        this.updateNews();
     }
-    }
+    
     
     render() {
         return (
