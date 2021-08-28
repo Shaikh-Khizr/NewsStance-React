18af9c8a20cc1f6b4d34fffc65b4718f2bb19e80
Khizr Abdul Ahad Shaikh <khizars949@gmail.com>
2021-08-28 22:03:51 +0530

    Video 32 of course and 11 of project completed.

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let {title, description, imageUrl, newsUrl} = this.props;
+        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
             <div className="my-3">
                 <div className="card">
-                    <img src={!imageUrl?"https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg":imageUrl} className="card-img-top" alt="..." />
+                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-dark" style={{left: '90%', zIndex: '1'}}>
+                        {!source ? "Unknown" : source}
+                    </span>
+                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}...</h5>
-                        <p className="card-text">{description}...</p>
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
                         <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
                     </div>
                 </div>
